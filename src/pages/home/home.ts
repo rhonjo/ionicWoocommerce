419ddc620dfb9a2cba979515ff65430fe7a0c3bc
Ryota Honjo <rhonjo@gmail.com>
2018-06-17 23:09:25 -0700

    set up woocommerce api and added more slides for dummy data products

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  WooCommerce: any;
+  products: any[];
 
+  @ViewChild('productSlides') productSlides: Slides;
+
+  constructor(public navCtrl: NavController) {
+    this.WooCommerce = WC({
+      url: "http://localhost:8080/wordpress/",
+      consumerKey: "ck_c03d01a3639301b1eb07b503cc33e3cb5032adfd",
+      consumerSecret: "cs_657da7da3c888c58cc034a9fa5bfd30779836c7b"
+    });
+
+    this.WooCommerce.getAsync("products").then((data) => {
+      console.log(JSON.parse(data.body));
+      this.products = JSON.parse(data.body).products;
+    }, (err) => {
+        console.log(err);
+    })
+
+  }
+
+  ionViewDidLoad() {
+    setInterval(() => {
+
+      if(this.productSlides.getActiveIndex() == this.productSlides.length() -1) {
+        this.productSlides.slideTo(0)
+      }
+
+      this.productSlides.slideNext();
+    }, 3000)
   }
 
 }
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
 @Component({
@@ -8,7 +8,7 @@ import { HomePage } from '../home/home';
 })
 export class Menu {
 
-  homePage: Component;
+  homePage: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
       this.homePage = HomePage
