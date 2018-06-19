b151404f321a7b18491f0350f8bf8e87e80ddcb3
Ryota Honjo <rhonjo@gmail.com>
2018-06-18 23:32:35 -0700

    added categories in the menu tab

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-menu',
@@ -9,9 +10,33 @@ import { HomePage } from '../home/home';
 export class Menu {
 
   homePage: any;
+  WooCommerce: any;
+  categories: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
       this.homePage = HomePage
+       this.categories = [];
+
+
+    this.WooCommerce = WC({
+      url: "http://localhost:8080/wordpress/",
+      consumerKey: "ck_c03d01a3639301b1eb07b503cc33e3cb5032adfd",
+      consumerSecret: "cs_657da7da3c888c58cc034a9fa5bfd30779836c7b"
+    });
+
+    this.WooCommerce.getAsync("products/categories").then((data) => {
+      console.log(JSON.parse(data.body).product_categories);
+
+      let temp: any[] = JSON.parse(data.body).product_categories;
+
+      for(let i = 0; i < temp.length; i++) {
+        if(temp[i].parent == 0) {
+          this.categories.push(temp[i])
+        }
+      }
+    }, (err)=> {
+      console.log(err)
+    })
   }
 
   ionViewDidLoad() {
