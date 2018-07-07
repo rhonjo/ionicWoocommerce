f85a03b709807cc973a10562b4a3b6cfc6971b5e
Ryota Honjo <rhonjo@gmail.com>
2018-07-07 15:33:10 -0700

    added more to product detail page with reviews and specification

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-// import * as WC from 'woocommerce-api';
+ import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-product-details',
@@ -9,8 +9,8 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ProductDetails {
 
   product: any;
-  // WooCommerce: any;
-  // reviews: any[] = [];
+  WooCommerce: any;
+  reviews: any[] = [];
   // selectedOptions: any = {};
   // requireOptions: boolean = true;
   // productVariations: any[] = [];
@@ -22,16 +22,20 @@ export class ProductDetails {
     this.product = this.navParams.get("product");
     console.log(this.product);
 
-    // this.WooCommerce = WP.init(true);
-    //
-    // this.WooCommerce.getAsync('products/' + this.product.id + '/reviews').then((data) => {
-    //
-    //   this.reviews = JSON.parse(data.body);
-    //   console.log(this.reviews);
-    //
-    // }, (err) => {
-    //   console.log(err);
-    // })
+     this.WooCommerce = WC({
+      url: "http://localhost:8080/wordpress/",
+      consumerKey: "ck_c03d01a3639301b1eb07b503cc33e3cb5032adfd",
+      consumerSecret: "cs_657da7da3c888c58cc034a9fa5bfd30779836c7b"
+    });
+
+     this.WooCommerce.getAsync('products/' + this.product.id + '/reviews').then((data) => {
+
+       this.reviews = JSON.parse(data.body).product_reviews;
+       console.log(this.reviews);
+
+     }, (err) => {
+       console.log(err);
+     })
 
   }
 
